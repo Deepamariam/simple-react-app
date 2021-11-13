b9ea85d179dcfdb199b309ee7b1ae038593a5de6
Deepamariam <godi2000kv@gmail.com>
2021-11-13 21:53:18 +0530

    added files.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+        console.log(data);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div class="jumbotron">
+        <h1 class="display-header">Blog posts</h1>
+      </div>
+      <div className="app-body">
+        {data.map((post) => (
+          <div className="card" key={post.id}>
+            <div className="card-header">
+              #{post.id} {post.title}
+            </div>
+            <div className="card-body">
+              <p className="card-text">{post.body}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
